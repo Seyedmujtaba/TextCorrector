2e30e42adeec329cc8ff2ff92896750d33ff63e3
Seyedmujtaba <seyedmujtaba.taba@gmail.com>
2025-09-24 00:13:39 +0330

    Update __init__.py

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,2 +1,13 @@
-#src/backend/__init__.py
-#Empty file to make this folder a Python package
+#--- writed by Seyedmujtaba Tabatabaee---#
+# src/utils/__init__.py
+
+from .text_utils import to_lowercase, split_words, clean_spaces, join_words
+from .dict_loader import load_dictionary
+
+__all__ = [
+    "to_lowercase",
+    "split_words",
+    "clean_spaces",
+    "join_words",
+    "load_dictionary",
+]
