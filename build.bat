9e484820b9e87812bd63c96e273b201baaa3141d
Seyedmujtaba <seyedmujtaba.taba@gmail.com>
2025-10-04 00:53:32 +0330

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,13 +4,18 @@ cd /d "%~dp0"
 
 echo === Using Python ===
 where python
-
 echo.
+
 echo === Running build.py ===
 python "%~dp0build.py"
 set EC=%ERRORLEVEL%
-
 echo.
 echo ExitCode: %EC%
-echo (اگر خطا دیدی، همین پنجره رو نبند؛ اسکرین‌شات بگیر)
-pause
+
+echo.
+echo === Dist listing ===
+dir /-c "%~dp0dist"
+
+echo.
+echo Press any key to exit...
+pause >nul
